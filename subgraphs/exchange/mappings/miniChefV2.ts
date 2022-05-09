12664af69de02dac7b164755d5c4e694c1e45e23
Brandon <brandon@pangolin.exchange>
2022-05-09 17:23:03 -0400

    Handle EmergencyWithdraw event

--- subgraphs/exchange/mappings/miniChefV2.ts
+++ subgraphs/exchange/mappings/miniChefV2.ts
@@ -9,7 +9,7 @@ import {
   createLiquiditySnapshot,
   createFarm,
 } from './helpers'
-import { Deposit, PoolAdded, Withdraw } from '../generated/MiniChefV2/MiniChefV2'
+import { Deposit, PoolAdded, Withdraw, EmergencyWithdraw } from '../generated/MiniChefV2/MiniChefV2'
 
 export function handlePoolAdded(event: PoolAdded): void {
   createFarm(event.address, event.params.pid, event.params.lpToken)
@@ -38,6 +38,28 @@ export function handleDeposit(event: Deposit): void {
 }
 
 export function handleWithdraw(event: Withdraw): void {
+  // user stats
+  createUser(event.params.to)
+
+  if (event.params.user.notEqual(event.params.to)) {
+    let farmKey = event.address.toHexString() + '-' + event.params.pid.toHexString()
+    let farm = Farm.load(farmKey)
+
+    let convertedAmount = convertTokenToDecimal(event.params.amount, BI_18)
+
+    let fromUserLiquidityPosition = createLiquidityPosition(farm.pairAddress as Address, event.params.user)
+    fromUserLiquidityPosition.liquidityTokenBalance = fromUserLiquidityPosition.liquidityTokenBalance.minus(convertedAmount)
+    fromUserLiquidityPosition.save()
+    createLiquiditySnapshot(fromUserLiquidityPosition, event)
+
+    let toUserLiquidityPosition = createLiquidityPosition(farm.pairAddress as Address, event.params.to)
+    toUserLiquidityPosition.liquidityTokenBalance = toUserLiquidityPosition.liquidityTokenBalance.plus(convertedAmount)
+    toUserLiquidityPosition.save()
+    createLiquiditySnapshot(toUserLiquidityPosition, event)
+  }
+}
+
+export function handleEmergencyWithdraw(event: EmergencyWithdraw): void {
   // user stats
   createUser(event.params.to)
 
